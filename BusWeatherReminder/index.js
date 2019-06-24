d063b08658ee93cd83b7e92d49eb4fa5df2fa1d0
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-06-24 20:31:11 +1200

    send message code but untest
    25/06/2019 TODO: more coding on js

--- BusWeatherReminder/index.js
+++ BusWeatherReminder/index.js
@@ -1,9 +1,29 @@
+//credentials
+const twilioAccountSid = 'AC98341b0926c068eae2229935485fae27';
+const twilioAuthToken = 'cee7c369dce679ba059880b8b8ccbdfb';
+const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
+const SMSTarget = '+6402102225648';
+const SMSSource = '+18123474471';
+
+//function entry 
 module.exports = async function (context, myTimer) {
+    //Default code
     var timeStamp = new Date().toISOString();
-    
-    if (myTimer.IsPastDue)
-    {
+    if (myTimer.IsPastDue) {
         context.log('JavaScript is running late!');
     }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+
+    //My code start here
+    context.log('[DEBUG LABLE: FUNCTION HEAD]');
+    sendMessage('Message testing');
+    return context.log('[DEBUG LABLE: FUNCTION END]');
 };
+
+//help function
+function sendMessage(messageBody) {
+    twilioClient.messages.create({
+        to: SMSTarget,
+        from: SMSSource,
+        body: current_hour + 'Just a 5 minute reminder to keep your posture up'
+    })
+}
