fb0069a51610cf87ad33cec2295cca12af548a98
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-06-25 23:00:19 +1200

    Revert "module desgin test"
    This reverts commit f7afba984d3f0450677ffe1ecd9a2f2e8449dd3f.

--- BusWeatherReminder/index.js
+++ BusWeatherReminder/index.js
@@ -1,9 +1,9 @@
 //credentials
-// const twilioAccountSid = 'AC98341b0926c068eae2229935485fae27';
-// const twilioAuthToken = 'cee7c369dce679ba059880b8b8ccbdfb';
-// const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
-// const SMSTarget = '+6402102225648';
-// const SMSSource = '+18123474471';
+const twilioAccountSid = 'AC98341b0926c068eae2229935485fae27';
+const twilioAuthToken = 'cee7c369dce679ba059880b8b8ccbdfb';
+const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
+const SMSTarget = '+6402102225648';
+const SMSSource = '+18123474471';
 
 //function entry 
 module.exports = async function (context, myTimer) {
@@ -12,19 +12,18 @@ module.exports = async function (context, myTimer) {
     if (myTimer.IsPastDue) {
         context.log('JavaScript is running late!');
     }
+
     //My code start here
     context.log('[DEBUG LABLE: FUNCTION HEAD]');
-//    sendMessage('Message testing');
-    var res = require('./messageSender.js');
-    res.data.Send(messageBody);
+    sendMessage('Message testing');
     return context.log('[DEBUG LABLE: FUNCTION END]');
 };
 
 //help function
-// function sendMessage(messageBody) {
-//     twilioClient.messages.create({
-//         to: SMSTarget,
-//         from: SMSSource,
-//         body:messageBody
-//     })
-// }
+function sendMessage(messageBody) {
+    twilioClient.messages.create({
+        to: SMSTarget,
+        from: SMSSource,
+        body:messageBody
+    })
+}
