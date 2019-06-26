c52be3a72de273470cb0eb4e9dd279a260a36134
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-06-26 19:54:40 +1200

    Update index.js

--- BusWeatherReminder/index.js
+++ BusWeatherReminder/index.js
@@ -10,14 +10,14 @@ module.exports = async function (context, myTimer) {
 
     weatherLoader.getJSON().then(
         function(result){
-            console.log('testing');
-            sender.sendBody(result.list[0].weather[0]+'Fucntion acting like object and exports to index.js');
+            context.log('testing');
+            context.log(result);
+
+            //sender.sendBody(result.list[0].weather[0]+'Fucntion acting like object and exports to index.js');
         }
     )
     //My code start here
     context.log('[DEBUG LABLE: FUNCTION HEAD]');
-
-    
 
     return context.log('[DEBUG LABLE: FUNCTION END]');
 };
