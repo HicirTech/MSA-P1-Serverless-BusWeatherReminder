ba28659c48fc5f2a1df9e2c2a660eb24628d5035
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-06-28 16:54:36 +1200

    Update MessageSender.js

--- BusWeatherReminder/MessageSender.js
+++ BusWeatherReminder/MessageSender.js
@@ -1,10 +1,14 @@
+const twilioAccountSid= process.env['twilioAccountSid'];
+const twilioAuthToken = process.env['twilioAuthToken'],
+
 var MessageSender = {
-    twilioAccountSid: process.env['twilioAccountSid'],
-    twilioAuthToken: process.env['twilioAuthToken'],
+ //   twilioAccountSid: process.env['twilioAccountSid'],
+  //  twilioAuthToken: process.env['twilioAuthToken'],
     SMSTarget: '+6402102225648',
     SMSSource: '+61437650299',
     sendBody: function (messageBody) {
-        twilioClient = require('twilio')(this.twilioAccountSid, this.twilioAuthToken);
+        twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
+        console.log(twilioAccountSid+twilioAuthToken);
         twilioClient.messages.create({
             to: this.SMSTarget,
             from: this.SMSSource,
