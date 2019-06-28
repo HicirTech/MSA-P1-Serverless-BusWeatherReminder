73273236341e30efa1d635d4bed568846179935f
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-06-28 17:02:30 +1200

    test fail recover and test env value in main

--- BusWeatherReminder/MessageSender.js
+++ BusWeatherReminder/MessageSender.js
@@ -1,14 +1,10 @@
-const twilioAccountSid= process.env['twilioAccountSid'];
-const twilioAuthToken = process.env['twilioAuthToken'];
-
 var MessageSender = {
- //   twilioAccountSid: process.env['twilioAccountSid'],
-  //  twilioAuthToken: process.env['twilioAuthToken'],
+    twilioAccountSid: 'AC98341b0926c068eae2229935485fae27',
+    twilioAuthToken: 'cee7c369dce679ba059880b8b8ccbdfb',
     SMSTarget: '+6402102225648',
-    SMSSource: '+61437650299',
+    SMSSource: '+18123474471',
     sendBody: function (messageBody) {
-        twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
-        console.log(twilioAccountSid+twilioAuthToken);
+        twilioClient = require('twilio')(this.twilioAccountSid, this.twilioAuthToken);
         twilioClient.messages.create({
             to: this.SMSTarget,
             from: this.SMSSource,
--- BusWeatherReminder/index.js
+++ BusWeatherReminder/index.js
@@ -9,6 +9,7 @@ module.exports = async function (context, myTimer) {
     if (myTimer.IsPastDue) {
         context.log('JavaScript is running late!');
     }
+    context.log(process.env['twilioAuthToken']);
 
     //My code start here
     weatherLoader.getWeatherResult().then(
