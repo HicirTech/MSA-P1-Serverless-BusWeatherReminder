f2c2dbce543083f629837c1bdcd41d1038b9a7e0
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-06-30 22:55:26 +1200

    Update jsonFaceLoader.js

--- BusWeatherReminder/jsonFaceLoader.js
+++ BusWeatherReminder/jsonFaceLoader.js
@@ -5,7 +5,7 @@ var faceLoader = {
     getARandomFace: function () {
         rawdata = fs.readFileSync('./textFace.json');
         faces = JSON.parse(rawdata);
-        return (faces.face[Math.floor(Math.random() * faces.face.length)]));
+        return (faces.face[Math.floor(Math.random() * faces.face.length)]);
     }
 }
 module.exports = faceLoader;
