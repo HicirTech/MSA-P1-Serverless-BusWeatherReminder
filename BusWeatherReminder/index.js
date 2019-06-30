90164574760af49c76a7845a4071268048a21fc0
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-06-30 23:22:44 +1200

    Update index.js

--- BusWeatherReminder/index.js
+++ BusWeatherReminder/index.js
@@ -27,7 +27,7 @@ module.exports = async function (context, myTimer) {
 
 
     //My code start here
-    weatherLoader.getWeatherResult(weatherApiKey).then(
+    context.log(weatherLoader.getWeatherResult(weatherApiKey).then(
         function (weatherResult) {
             let resultString = weatherStringMakeUp(weatherResult);
             atLoader.getTimeTable(ATkey).then(
@@ -36,7 +36,7 @@ module.exports = async function (context, myTimer) {
                     sender.sendBody(twilioAccountSid, twilioAuthToken, resultString);
                 });
         }
-    );
+    ));
 };
 
 //helpers
